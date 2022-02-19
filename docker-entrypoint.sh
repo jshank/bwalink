2dc675b2b49cea2ff28a594d8dbebd45f8e128c3
Jim Shank <jim.shank@gmail.com>
2022-02-19 11:10:25 -0800

    Make socat more resilient

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 #Start-up socat to bind the remote serial to IP relay to a virtual port on /dev/hottub
-socat pty,link=/dev/hottub,b115200,raw,echo=0 tcp4:${BRIDGE_IP}:${BRIDGE_PORT} &
+socat pty,link=/dev/hottub,b115200,raw,echo=0 tcp4:${BRIDGE_IP}:${BRIDGE_PORT},forever,interval=10,fork &
 
 #Launch the bwa_mqtt_bridge 
 /usr/local/bundle/bin/bwa_mqtt_bridge ${MQTT_URI} /dev/hottub
